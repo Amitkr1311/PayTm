5ad76e23422ca65125809a1d46f0ead116c20527
Amitkr1311 <1311amitkr@gmail.com>
2025-10-22 05:08:14 +0530

    removed url

--- backend/db.js
+++ backend/db.js
@@ -2,8 +2,6 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 import { MONGO_URL } from "./config.js";
 
-// mongoose.connect("mongodb+srv://1311amitkr:vGGLtfR3sokm2aEy@cluster0.5ghknrp.mongodb.net/paytm");
-
 mongoose.connect(MONGO_URL)
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch((err) => console.error("MongoDB connection error:", err));
